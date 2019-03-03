5ba2f14a22a2b982c7db0254b1afa27fad3af215
= <therealmerengue@gmail.com>
2019-03-03 14:10:01 +0100

    Online/offline message box.

--- lib/pages/converter/converter_page.dart
+++ lib/pages/converter/converter_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kiwi/kiwi.dart' as dependencies;
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:minimalist_converter/common/models/currency.dart';
 import 'package:minimalist_converter/common/models/currency_display_type.dart';
 import 'package:minimalist_converter/common/values/colors.dart';
@@ -18,7 +19,8 @@ class ConverterPage extends StatefulWidget {
   _ConverterPageState createState() => _ConverterPageState();
 }
 
-class _ConverterPageState extends State<ConverterPage> {
+class _ConverterPageState extends State<ConverterPage>
+    with TickerProviderStateMixin {
   final CurrencyDisplayBloc _redDisplayBloc = dependencies.Container()
       .resolve<CurrencyDisplayBloc>("red_currency_display_bloc");
 
@@ -126,14 +128,72 @@ class _ConverterPageState extends State<ConverterPage> {
     ));
   }
 
+  //TODO: dispatch last action after going back online again if it failed before because of no internet
+  //TODO: make online message disappear after some time -> use animated widget for it - tune the animation
+  Widget _mainBody(BuildContext context) {
+    return OfflineBuilder(
+      connectivityBuilder: (
+        BuildContext context,
+        ConnectivityResult connectivity,
+        Widget child,
+      ) {
+        final connected = connectivity != ConnectivityResult.none;
+        final connectionInfoBox = Positioned(
+          bottom: 0.0,
+          left: 0.0,
+          right: 0.0,
+          height: 24.0,
+          child: Container(
+            color: connected ? Colors.green : Colors.red,
+            child: Center(
+              child: Text(
+                "${connected ? 'ONLINE' : 'OFFLINE'}",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        );
+        if (connected) {
+          _onlineMessageAnimationController.forward(from: 0.0);
+          final onlineMessage = _OnlineMessage(
+            connectionInfoBox,
+            _onlineMessageVisibilityDuration,
+            animation: StepTween(
+              begin: 0,
+              end: _onlineMessageVisibilityDuration,
+            ).animate(_onlineMessageAnimationController),
+          );
+          return Stack(
+            children: <Widget>[child, onlineMessage],
+          );
+        } else {
+          return Stack(
+            children: <Widget>[child, connectionInfoBox],
+          );
+        }
+      },
+      child: Stack(
+        children: <Widget>[_mainDisplay(context), _arrowButton(context)],
+      ),
+    );
+  }
+
+  final int _onlineMessageVisibilityDuration = 6;
+  AnimationController _onlineMessageAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _onlineMessageAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: _onlineMessageVisibilityDuration),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Material(
-        child: Stack(
-          children: <Widget>[_mainDisplay(context), _arrowButton(context)],
-        ),
-      ),
+      body: Material(child: _mainBody(context)),
     );
   }
 
@@ -142,5 +202,24 @@ class _ConverterPageState extends State<ConverterPage> {
     _redDisplayBloc.dispose();
     _whiteDisplayBloc.dispose();
     super.dispose();
+  }
+}
+
+class _OnlineMessage extends AnimatedWidget {
+  final Animation<int> animation;
+  final Widget _child;
+  final int _animationDurationSeconds;
+
+  _OnlineMessage(
+    this._child,
+    this._animationDurationSeconds, {
+    Key key,
+    @required this.animation,
+  }) : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    if (animation.value < _animationDurationSeconds) return _child;
+    return Container();
   }
 }
